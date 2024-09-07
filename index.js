85b90036978e63d9ef0e7c3fc48c9d9818f6e183
gm <no@email.provided>
2024-09-06 21:21:41 -0700

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const url = "https://api-web.nhle.com/v1/player/8478402/landing";
+
+const options = {
+  headers: {
+    Authorization: "Bearer 6Q************"
+  }
+};
+
+fetch(url, options)
+  .then( res => res.json() )
+  .then( data => console.log(data) );
